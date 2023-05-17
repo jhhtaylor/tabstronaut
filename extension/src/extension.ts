3f549a1d03ef70fd43465b63ea43bc3c057702e2
Jon Taylor <jon.taylor@entelect.co.za>
2023-05-17 18:59:52 +0200

    Can add current tab to sidebar custom view

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,7 +1,29 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 import { TabstronautDataProvider } from './tabstronautDataProvider';
 
+let treeDataProvider: TabstronautDataProvider;
+
 export function activate(context: vscode.ExtensionContext) {
-	const treeDataProvider = new TabstronautDataProvider();
+	treeDataProvider = new TabstronautDataProvider();
 	vscode.window.createTreeView('tabstronaut', { treeDataProvider });
+
+	const statusBarCommand = 'tabstronaut.addCurrentTab';
+	const statusBarButtonText = '$(plus) Add to Tabstronaut';
+
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	statusBarItem.text = statusBarButtonText;
+	statusBarItem.command = statusBarCommand;
+	statusBarItem.show();
+	context.subscriptions.push(statusBarItem);
+
+	let disposable = vscode.commands.registerCommand(statusBarCommand, () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+			const filePath = activeEditor.document.fileName;
+			const fileName = path.basename(filePath);
+			treeDataProvider.addItem(fileName);
+		}
+	});
+	context.subscriptions.push(disposable);
 }
--- extension/src/tabstronautDataProvider.ts
+++ extension/src/tabstronautDataProvider.ts
@@ -1,17 +1,33 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+
+// const fileIcons: { [extension: string]: vscode.ThemeIcon } = {
+//     '.js': new vscode.ThemeIcon('file-type-javascript'),
+//     '.ts': new vscode.ThemeIcon('file-type-typescript'),
+//     '.sql': new vscode.ThemeIcon('file-type-sql'),
+//     // add more mappings as needed
+// };
 
 export class TabstronautDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<vscode.TreeItem | undefined | null | void> = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
+    readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+
+    private items: vscode.TreeItem[] = [];
+
     getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
         return element;
     }
 
     getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
-        if (element) {
-            return Promise.resolve([]);
-        } else {
-            return Promise.resolve([
-                new vscode.TreeItem('Hello from Tabstronaut2!')
-            ]);
-        }
+        return Promise.resolve(this.items);
+    }
+
+    addItem(label: string) {
+        //const extension = path.extname(label);
+        //const icon = fileIcons[extension] || new vscode.ThemeIcon('file');
+        const item = new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.None);
+        item.iconPath = new vscode.ThemeIcon('file');
+        this.items.push(item);
+        this._onDidChangeTreeData.fire();
     }
 }
