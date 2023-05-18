ee74e08e4df8b42554bdfffc7f9b0508b91c1006
Jon Taylor <jon.taylor@entelect.co.za>
2023-05-18 14:08:00 +0200

    Clean up code

--- extension/src/tabstronautDataProvider.ts
+++ extension/src/tabstronautDataProvider.ts
@@ -1,12 +1,4 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-
-// const fileIcons: { [extension: string]: vscode.ThemeIcon } = {
-//     '.js': new vscode.ThemeIcon('file-type-javascript'),
-//     '.ts': new vscode.ThemeIcon('file-type-typescript'),
-//     '.sql': new vscode.ThemeIcon('file-type-sql'),
-//     // add more mappings as needed
-// };
 
 export class TabstronautDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
     private _onDidChangeTreeData: vscode.EventEmitter<vscode.TreeItem | undefined | null | void> = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
@@ -23,8 +15,6 @@ export class TabstronautDataProvider implements vscode.TreeDataProvider<vscode.T
     }
 
     addItem(label: string) {
-        //const extension = path.extname(label);
-        //const icon = fileIcons[extension] || new vscode.ThemeIcon('file');
         const item = new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.None);
         item.iconPath = new vscode.ThemeIcon('file');
         this.items.push(item);
