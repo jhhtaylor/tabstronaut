ce047674d876f764ccc1e119476250e881404613
Jon Taylor <jon.taylor@entelect.co.za>
2023-05-24 17:22:52 +0200

    Removed Svelte

--- extension/media/main.js
+++
@@ -1,7 +0,0 @@
-// This script will be run within the webview itself
-// It cannot access the main VS Code APIs directly.
-(function () {
-  const vscode = acquireVsCodeApi();
-
-  console.log("hello there from javascript");
-})();
