42f3c7e9b5a3ceb7c20c5f9aded081b3b5ac3e04
Jon Taylor <jon.taylor@entelect.co.za>
2023-08-19 11:51:22 +0200

    Can just click on tab to open it

--- extension/src/models/Group.ts
+++ extension/src/models/Group.ts
@@ -30,6 +30,13 @@ export class Group extends vscode.TreeItem {
         item.id = this.id + filePath;
         item.contextValue = 'tab';
 
+        // Add the command to open the tab when the item is clicked
+        item.command = {
+            command: 'tabstronaut.openSpecificTab',
+            title: 'Open Specific Tab',
+            arguments: [item]
+        };
+
         this.items.push(item);
     }
 }
